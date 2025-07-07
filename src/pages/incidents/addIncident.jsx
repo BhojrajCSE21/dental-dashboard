11a6e014c8143b08192d904628e3f3773f02c4f1
BhojrajCSE21 <bhojrajnarware@gmail.com>
2025-07-07 22:36:32 +0530

    ok

--- src/pages/incidents/addIncident.jsx
+++ src/pages/incidents/addIncident.jsx
@@ -196,7 +196,7 @@ const AddIncident = () => {
         </fieldset>
         <div className="flex justify-end">
           <button type="submit" className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
-            Book Appointment
+            Book Appointment 
           </button>
         </div>
       </form>
